3d07c04abe890a4ee64ca057af1188a100fa875d
Aniket Nigam <aniknigam2@gmail.com>
2024-02-24 00:48:25 +0530

    service class completed for users

--- UserService/src/main/java/com/lcdw/user/service/entities/Users.java
+++ UserService/src/main/java/com/lcdw/user/service/entities/Users.java
@@ -5,13 +5,49 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name = "user")
 public class Users {
     @Id
     private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
     @Column
     private String name;
     @Column
--- UserService/src/main/java/com/lcdw/user/service/serviceImpl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcdw/user/service/serviceImpl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lcdw.user.service.serviceImpl;
 
 import com.lcdw.user.service.entities.Users;
+import com.lcdw.user.service.excepiton.ResourceNotFoundException;
 import com.lcdw.user.service.repository.UserRepository;
 import com.lcdw.user.service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,16 +27,20 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Users getUser(String uId) {
-        return null;
+        return userRepository.findById(uId).orElseThrow(() -> new ResourceNotFoundException("User with id not found"));
     }
 
     @Override
     public Users updateUser(Users user) {
+        Users update = new Users();
+        update.setName(user.getName());
+        update.setEmail(user.getEmail());
+        update.setAbout(user.getAbout());
         return null;
     }
 
     @Override
     public void delete(String userid) {
-
+     userRepository.deleteById(userid);
     }
 }
