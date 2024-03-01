586ab647746f41bcb00d28a4958354b22fbe3888
Aniket Nigam <aniknigam2@gmail.com>
2024-03-01 13:35:41 +0530

    rest template added in user service for fetching ratings and hotel both

--- UserService/src/main/java/com/lcdw/user/service/entities/Ratings.java
+++ UserService/src/main/java/com/lcdw/user/service/entities/Ratings.java
@@ -9,4 +9,54 @@ public class Ratings {
     private String hotelId;
     private String feedback;
     private int rating;
+
+    private Hotel hotel;
+
+    public String getRatingId() {
+        return ratingId;
+    }
+
+    public void setRatingId(String ratingId) {
+        this.ratingId = ratingId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(String hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
 }
--- UserService/src/main/java/com/lcdw/user/service/serviceImpl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcdw/user/service/serviceImpl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lcdw.user.service.serviceImpl;
 
+import com.lcdw.user.service.entities.Hotel;
 import com.lcdw.user.service.entities.Ratings;
 import com.lcdw.user.service.entities.Users;
 import com.lcdw.user.service.excepiton.ResourceNotFoundException;
@@ -7,11 +8,15 @@ import com.lcdw.user.service.repository.UserRepository;
 import com.lcdw.user.service.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -26,6 +31,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Users saveUser(Users usrs) {
+        String create =   UUID.randomUUID().toString();
+        usrs.setUserId(create);
         return userRepository.save(usrs);
     }
 
@@ -38,10 +45,29 @@ public class UserServiceImpl implements UserService {
     public Users getUser(String uId) {
         //rating url
         //http://localhost:8084/ratings/user/uid
-        ArrayList<Ratings> data = restTemplate.getForObject("http://localhost:8084/ratings/user/"+uId, ArrayList.class);
-        System.out.println(data);
+
         Users user =  userRepository.findById(uId).orElseThrow(() -> new ResourceNotFoundException("User with id not found"));
-        user.setRatings(data);
+        Ratings[] data = restTemplate.getForObject("http://localhost:8084/ratings/user/"+uId, Ratings[].class);
+        System.out.println(data);
+
+        List<Ratings> rating = Arrays.stream(data).toList();
+
+        List<Ratings> ratingsList = rating.stream().map( ratings -> {
+            /*
+            * Steps
+            * 1. api call to hotel service to get the hotel
+            * 2. set the hotel to rating
+            * 3. return the rating
+            * */
+          String getHotelByidUrl = "http://localhost:8081/hotels/getHotel/"+ ratings.getHotelId();
+        //    String getHotelByidUrl = "http://localhost:8081/hotels/getHotel/5b8d4832-5ba7-4d84-b65f-c10566d89c49";
+            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity(getHotelByidUrl, Hotel.class);
+            Hotel hotel = forEntity.getBody();
+            ratings.setHotel(hotel);
+            return  ratings;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratingsList);
 
         return user;
     }
